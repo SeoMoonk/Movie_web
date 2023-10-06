2e43d12d196b8e7f8c2e8ca0edbab9ed16755301
SeoMoonk <sunghoon1216@skuniv.ac.kr>
2023-10-06 14:02:08 +0900

    Enhancement: normal movie list page

--- src/main/java/com/seomoon/movieApp/boundedContext/movie/controller/MovieController.java
+++ src/main/java/com/seomoon/movieApp/boundedContext/movie/controller/MovieController.java
@@ -1,12 +1,30 @@
 package com.seomoon.movieApp.boundedContext.movie.controller;
 
+import com.seomoon.movieApp.boundedContext.movie.entity.Movie;
 import com.seomoon.movieApp.boundedContext.movie.service.MovieService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/movie")
 public class MovieController {
 
     private final MovieService movieService;
+
+    @GetMapping("/list")
+    public String getMovieList(Model model) {
+
+        List<Movie> allMovieList = movieService.getAllMovieList();
+
+        model.addAttribute("movieList", allMovieList);
+
+        return "view/movie/movieList";
+    }
+
 }
--- src/main/java/com/seomoon/movieApp/boundedContext/movie/service/MovieService.java
+++ src/main/java/com/seomoon/movieApp/boundedContext/movie/service/MovieService.java
@@ -1,9 +1,12 @@
 package com.seomoon.movieApp.boundedContext.movie.service;
 
+import com.seomoon.movieApp.boundedContext.movie.entity.Movie;
 import com.seomoon.movieApp.boundedContext.movie.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -11,5 +14,10 @@ import org.springframework.transaction.annotation.Transactional;
 public class MovieService {
 
     private final MovieRepository movieRepository;
+
+    public List<Movie> getAllMovieList() {
+
+        return movieRepository.findAll();
+    }
 
 }
