5c74767e7f2df72d226628618d4f62920324e3ce
SeoMoonk <sunghoon1216@skuniv.ac.kr>
2023-10-06 09:33:25 +0900

    init

---
+++ src/main/java/com/seomoon/movieApp/boundedContext/member/entity/Member.java
@@ -0,0 +1,19 @@
+package com.seomoon.movieApp.boundedContext.member.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@SuperBuilder(toBuilder = true)
+@ToString(callSuper = true)
+public class Member {
+
+}
---
+++ src/main/java/com/seomoon/movieApp/boundedContext/member/entity/MemberGrade.java
@@ -0,0 +1,15 @@
+package com.seomoon.movieApp.boundedContext.member.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum MemberGrade {
+
+    NORMAL_USER("일반 관람객"),
+    CRITIC_USER("전문 평론가"),
+    ADMIN_USER("극장 관리자");
+
+    private String grade;
+}
