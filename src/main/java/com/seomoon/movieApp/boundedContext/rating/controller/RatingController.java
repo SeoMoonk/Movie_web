2db3274b071be7f5d2f91dea012c251094391704
SeoMoonk <sunghoon1216@skuniv.ac.kr>
2023-10-06 17:44:38 +0900

    rating score form ing ...

--- src/main/java/com/seomoon/movieApp/boundedContext/rating/controller/RatingController.java
+++ src/main/java/com/seomoon/movieApp/boundedContext/rating/controller/RatingController.java
@@ -2,10 +2,14 @@ package com.seomoon.movieApp.boundedContext.rating.controller;
 
 import com.seomoon.movieApp.boundedContext.rating.service.RatingService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/rating")
 public class RatingController {
 
     private final RatingService ratingService;
