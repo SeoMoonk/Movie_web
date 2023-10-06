b40867f2057060670ec8da9cf3e206492b11bc8b
SeoMoonk <sunghoon1216@skuniv.ac.kr>
2023-10-06 14:39:11 +0900

    Enhancement: Custom Error Page

--- src/main/java/com/seomoon/movieApp/boundedContext/movie/controller/MovieController.java
+++ src/main/java/com/seomoon/movieApp/boundedContext/movie/controller/MovieController.java
@@ -3,9 +3,11 @@ package com.seomoon.movieApp.boundedContext.movie.controller;
 import com.seomoon.movieApp.boundedContext.movie.entity.Movie;
 import com.seomoon.movieApp.boundedContext.movie.service.MovieService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -25,6 +27,20 @@ public class MovieController {
         model.addAttribute("movieList", allMovieList);
 
         return "view/movie/movieList";
+    }
+
+    @GetMapping("/add")
+    @PreAuthorize("hasAuthority('admin')")
+    public String getAddMovieForm() {
+
+        return "/view/movie/addMovieForm";
+    }
+
+    @PostMapping("/add")
+    @PreAuthorize("hasAuthority('admin')")
+    public String addMovie() {
+
+        return "redirect:/movie/list";
     }
 
 }
